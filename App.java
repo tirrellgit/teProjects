716a0bda0d111c56d40c9fad76d65b652c763090
Tirrell Harris <157651715+tirrellgit@users.noreply.github.com>
2024-02-04 23:53:24 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,328 @@
+package com.lendingcatalog;
+
+import com.lendingcatalog.model.*;
+import com.lendingcatalog.util.FileStorageService;
+import com.lendingcatalog.util.exception.FileStorageException;
+
+import javax.xml.catalog.Catalog;
+import java.io.File;
+import java.time.LocalDate;
+import java.util.*;
+
+public class App {
+
+    // The regex string to split the Strings in the dataset.
+    private static final String FIELD_DELIMITER = "\\|";
+    private static final String FILE_BASE_PATH = "src/main/resources/";
+
+    private final Scanner keyboard = new Scanner(System.in);
+
+    private Map<String, List<CatalogItem>> catalog = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        App app = new App();
+        app.initialize();
+        app.run();
+    }
+
+    private void initialize() {
+        // Requirement: Data transformation
+        String memberFileDataPath = "src/main/resources/members.dat";
+        List<String> memberInfo = new ArrayList<>();
+
+        try {
+            memberInfo = FileStorageService.readContentsOfFile(memberFileDataPath);
+
+        } catch (FileStorageException e) {
+            throw new RuntimeException(e);
+        }
+        for (String memberLine : memberInfo) {
+            String[] singleMemberData = memberLine.split(FIELD_DELIMITER);
+            String firstName = singleMemberData[0];
+            String lastName = singleMemberData[1];
+            String fileName = singleMemberData[2];
+            Member member = new Member(firstName, lastName);
+
+            String dataFileName = FILE_BASE_PATH + fileName;
+            List<CatalogItem> memberItems = processMemberItems(dataFileName);
+            catalog.put(member.toString(), memberItems);
+
+        }
+
+
+    }
+
+    private List<CatalogItem> processMemberItems(String dataFileName) {
+        // return all CatalogItems for a single user
+        List<CatalogItem> singleMemberItems = new ArrayList<>();
+
+        //use FileStorageService to get a list of strings, each of which represents one CatalogItem
+        List<String> catalogItemInfo = new ArrayList<>();
+        try {
+            catalogItemInfo = FileStorageService.readContentsOfFile(dataFileName);
+        } catch (FileStorageException e) {
+            System.out.println("File Storage Exception");
+        }
+        for (String singleCatalogItem : catalogItemInfo) {
+            //break each line into it's component parts (catalog type, fields for catalog item except id)
+            String[] singleItem = singleCatalogItem.split(FIELD_DELIMITER);
+            if (singleItem[0].equals("book")) {
+                // its a book
+                // call on book constructor
+                // register item
+                // add finished item to SingleMemberItems
+                String title = singleItem[1];
+                String author = singleItem[2];
+                LocalDate publishDate = LocalDate.parse(singleItem[3]);
+                Book book = new Book(title, author, publishDate);
+                book.registerItem();
+                singleMemberItems.add(book);
+
+            } else if (singleItem[0].equals("movie")) {
+                // its a movie
+                // call on book constructor
+                // register item
+                // add finished item to SingleMemberItems
+                String name = singleItem[1];
+                String director = singleItem[2];
+                LocalDate publishDate = LocalDate.parse(singleItem[3]);
+                Movie movie = new Movie(name, director, publishDate);
+                movie.registerItem();
+                singleMemberItems.add(movie);
+
+            } else if (singleItem[0].equals("tool")) {
+                // its a tool
+                // call on book constructor
+                // register item
+                // add finished item to SingleMemberItems
+                String type = singleItem[1];
+                String manufacturer = singleItem[2];
+                int count = Integer.parseInt(singleItem[3]);
+                Tool tool = new Tool(type, manufacturer, count);
+                tool.registerItem();
+                singleMemberItems.add(tool);
+
+            } else {
+                System.err.println("Items not entered correctly");
+            }
+        }
+
+
+        return singleMemberItems;
+    }
+
+
+    private void run() {
+
+        if (catalog.isEmpty()) {
+            System.out.println("Catalog must not empty for application to run");
+            return;
+        }
+
+        while (true) {
+            // Main menu loop
+            printMainMenu();
+            int mainMenuSelection = promptForMenuSelection("Please choose an option: ");
+            if (mainMenuSelection == 1) {
+                // Display data and subsets loop
+                while (true) {
+                    printDataAndSubsetsMenu();
+                    int dataAndSubsetsMenuSelection = promptForMenuSelection("Please choose an option: ");
+                    if (dataAndSubsetsMenuSelection == 1) {
+                        displayFullCatalog(catalog);
+                    } else if (dataAndSubsetsMenuSelection == 2) {
+                        displayUsersForItemDisplay(catalog);
+                        String userName = promptForString("Enter name: ");
+                        displayUserItems(catalog, userName);
+                    } else if (dataAndSubsetsMenuSelection == 0) {
+                        break;
+                    }
+                }
+            } else if (mainMenuSelection == 2) {
+                while (true) {
+                    printSearchMenu();
+                    int searchMenuSelection = promptForMenuSelection("Please choose an option: ");
+                    if (searchMenuSelection == 1) {
+                        // Search by name/title
+                        String searchName = promptForString("Enter name: ");
+                        displayMatchesByName(catalog, searchName);
+                    } else if (searchMenuSelection == 2) {
+                        // Search by creator
+                        String searchCreator = promptForString("Enter creator: ");
+                        displayMatchesByCreator(catalog, searchCreator);
+                    } else if (searchMenuSelection == 3) {
+                        // Search by publish/release year
+                        int searchYear = promptForYear("Enter date (YYYY): ");
+                        displayMatchesByYear(catalog, searchYear);
+                    } else if (searchMenuSelection == 0) {
+                        break;
+                    }
+                }
+            } else if (mainMenuSelection == 0) {
+                break;
+            }
+        }
+    }
+
+
+    // UI methods
+
+    private void printMainMenu() {
+        System.out.println("1: Display catalog");
+        System.out.println("2: Search catalog");
+        System.out.println("0: Exit");
+        System.out.println();
+    }
+
+    private void printDataAndSubsetsMenu() {
+        System.out.println("1: Display full catalog");
+        System.out.println("2: Display all items from a user");
+        System.out.println("0: Return to main menu");
+        System.out.println();
+    }
+
+    private void printSearchMenu() {
+        System.out.println("1: Search items by name");
+        System.out.println("2: Search items by creator");
+        System.out.println("3: Search by year");
+        System.out.println("0: Return to main menu");
+        System.out.println();
+    }
+
+    private void displayFullCatalog(Map<String, List<CatalogItem>> catalog) {
+        System.out.println("Full Catalog");
+        System.out.println("-------");
+        for (Map.Entry<String, List<CatalogItem>> entry : catalog.entrySet()) {
+            System.out.println(entry.getKey() + ": ");
+            for (CatalogItem item : entry.getValue()) {
+                System.out.println(item.toString());
+            }
+            System.out.println();
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private void displayUsersForItemDisplay(Map<String, List<CatalogItem>> catalog) {
+        System.out.println("Users");
+        System.out.println("-------");
+        for (Map.Entry<String, List<CatalogItem>> entry : catalog.entrySet()) {
+            System.out.println(entry.getKey());
+        }
+        System.out.println();
+    }
+
+    private void displayUserItems(Map<String, List<CatalogItem>> catalog, String userName) {
+        System.out.println("Items from " + userName);
+        System.out.println("-------");
+        if (catalog.containsKey(userName)) {
+            List<CatalogItem> userItems = catalog.get(userName);
+            for (CatalogItem item : userItems) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("No user found for '" + userName + "'");
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private void displayMatchesByName(Map<String, List<CatalogItem>> catalog, String searchName) {
+        System.out.println("Matches by name: " + searchName);
+        System.out.println("-------");
+        for (Map.Entry<String, List<CatalogItem>> entry : catalog.entrySet()) {
+            boolean hasMatches = false;
+            System.out.println(entry.getKey() + ": ");
+            for (CatalogItem item : entry.getValue()) {
+                if (item.matchesName(searchName)) {
+                    System.out.println(item);
+                    hasMatches = true;
+                }
+            }
+            if (!hasMatches) {
+                System.out.println("--No matching items--");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private void displayMatchesByCreator(Map<String, List<CatalogItem>> catalog, String searchCreator) {
+        System.out.println("Matches by creator: " + searchCreator);
+        System.out.println("-------");
+        for (Map.Entry<String, List<CatalogItem>> entry : catalog.entrySet()) {
+            boolean hasMatches = false;
+            System.out.println(entry.getKey() + ": ");
+            for (CatalogItem item : entry.getValue()) {
+                if (item.matchesCreator(searchCreator)) {
+                    System.out.println(item);
+                    hasMatches = true;
+                }
+            }
+            if (!hasMatches) {
+                System.out.println("--No matching items--");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private void displayMatchesByYear(Map<String, List<CatalogItem>> catalog, int searchYear) {
+        System.out.println("Matches by year: " + searchYear);
+        System.out.println("-------");
+        for (Map.Entry<String, List<CatalogItem>> entry : catalog.entrySet()) {
+            boolean hasMatches = false;
+            System.out.println(entry.getKey() + ": ");
+            for (CatalogItem item : entry.getValue()) {
+                if (item.matchesYear(searchYear)) {
+                    System.out.println(item);
+                    hasMatches = true;
+                }
+            }
+            if (!hasMatches) {
+                System.out.println("--No matching items--");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        promptForReturn();
+    }
+
+    private int promptForMenuSelection(String prompt) {
+        System.out.print(prompt);
+        int menuSelection;
+        try {
+            menuSelection = Integer.parseInt(keyboard.nextLine());
+        } catch (NumberFormatException e) {
+            menuSelection = -1;
+        }
+        return menuSelection;
+    }
+
+    private String promptForString(String prompt) {
+        System.out.print(prompt);
+        return keyboard.nextLine();
+    }
+
+    private int promptForYear(String prompt) {
+        int year;
+        while (true) {
+            System.out.println(prompt);
+            try {
+                year = Integer.parseInt(keyboard.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("The year provided is not well-formed. It must be YYYY.");
+            }
+        }
+        return year;
+    }
+
+    private void promptForReturn() {
+        System.out.println("Press RETURN to continue.");
+        keyboard.nextLine();
+    }
+}
